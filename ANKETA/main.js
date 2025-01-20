e511b2832cdc2205ad1073897f5cac1fe8065931
Anna Kharitonchik <zharitonchik@icloud.com>
2025-01-21 00:21:40 +0300

    add new variable

--- ANKETA/main.js
+++ ANKETA/main.js
@@ -1,39 +1,40 @@
-let SURNAME = prompt("Введите фамилию");
+const SURNAME = prompt("Введите фамилию");
 while (!SURNAME) {
   alert("Введите корректную фамилию");
   SURNAME = prompt("Введите фамилию");
 }
-let NAME = prompt("Введите имя");
+const NAME = prompt("Введите имя");
 while (!NAME) {
   alert("Введите корректное имя");
   NAME = prompt("Введите корректное имя");
 }
-let MIDDLE_NAME = prompt("Введите отчество");
+const MIDDLE_NAME = prompt("Введите отчество");
 while (!MIDDLE_NAME) {
   alert("Введите корректное отчество");
   MIDDLE_NAME = prompt("Введите отчество");
 }
-let AGE = parseInt(prompt("Введите возраст"));
+const AGE = parseInt(prompt("Введите возраст"));
 while (AGE == 0 || Number.isNaN(AGE)) {
   alert("Введите корректный возраст");
   AGE = parseInt(prompt("Введите возраст"));
 }
-let GENDER = confirm(
+const genderQuestion = confirm(
   "Ваш пол - мужской? (нажмите OK, если да и нажмите ОТМЕНА, если нет)"
 );
 let pension;
+let gender;
 
 //Пусть женщины выходят на пенсию в 55, а мужчины в 60
 
-if (GENDER) {
-  GENDER = "мужской";
+if (genderQuestion) {
+  gender = "мужской";
   if (AGE < 60) {
     pension = "нет";
   } else {
     pension = "да";
   }
 } else {
-  GENDER = "женский";
+  gender = "женский";
   if (AGE < 55) {
     pension = "нет";
   } else {
@@ -47,5 +48,5 @@ alert(`ваше ФИО: ${SURNAME} ${NAME} ${MIDDLE_NAME}\n
 ваш возраст в годах: ${AGE}\n
 ваш возраст в днях: ${AGE * 365}\n
 через 5 лет вам будет: ${AGE + 5}\n
-ваш пол: ${GENDER}\n
+ваш пол: ${gender}\n
 вы на пенсии: ${pension}`);
